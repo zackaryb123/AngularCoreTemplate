458f838485797a8199b995236f1697acba449258
zackb <zackblaylock@gmail.com>
2022-01-21 10:47:37 -0500

    Quickbooks Paya form

---
+++ src/app/constants/index.ts
@@ -0,0 +1,4 @@
+export class AppConstants {
+  public static PAYA_REDIRECT = 'https://partners.paya.com/login/';
+
+}
--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {DOCUMENT} from '@angular/common';
+import {AppConstants} from '../../constants';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +9,11 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
-  constructor() { }
+  constructor(private _router: Router,
+              @Inject(DOCUMENT) private document: Document
+  ) { }
 
+  redirectPayaLogin() {
+    this.document.location.href = AppConstants.PAYA_REDIRECT;
+  }
 }
