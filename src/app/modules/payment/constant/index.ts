d49d622cec6318de283c1417473c8ca6ff544d77
zackaryb123 <zackblaylock@gmail.com>
2022-04-27 02:55:34 -0400

    Payment Scan to Pay - UI  update - enforce 80% quality

--- src/app/modules/payment/constant/index.ts
+++ src/app/modules/payment/constant/index.ts
@@ -4,8 +4,8 @@ export const CONSTANT = {
   },
   PAYMENT_CARD_IMAGE: {
     MAX_FILE_SIZE: 1048576,
-    COMPRESS_QUALITY: 50,
-    COMPRESS_RATIO: 50,
+    COMPRESS_QUALITY: 80,
+    COMPRESS_RATIO: 80,
     ORIENTATION: -1,
     TYPE: 'image/jpeg'
   }
